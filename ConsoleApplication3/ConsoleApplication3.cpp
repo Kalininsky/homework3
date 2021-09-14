241bdaa89379aa921e3024306674ef883a8bf2ee
Kirill <Kalininsky14@mail.ru>
2021-09-14 18:30:53 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	int a, b, c, d;
+	a = 7;
+	b = 23;
+	c = 125;
+	d = 14;
+	float f = a * (b + (float(c) / d));
+	std::cout << std::endl;
+
+	{
+
+		int a, b;
+		a = 25;
+		b = (a <= 21) ? (21 - a) : 2 * (a - 21);
+		std::cout << b << std::endl;
+	}
+}
